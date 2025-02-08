723a77cf02ec9621ac4b5b4c31ebf3b56733ebfa
Anne Brown <anne@browndomain.com>
2025-02-08 08:23:35 -0500

    Missing tag

--- apps/mdc-component-trims-md/pages/index.vue
+++ apps/mdc-component-trims-md/pages/index.vue
@@ -1,22 +1,38 @@
 <!--------@/pages/index.vue---------------------------------------------------->
-<template>
-  <div class="p-4">
+<template><div>
+<NuxtLayout name="splash">
+
+<div class="p-4">
     <!-- Title -->
     <h1>mdc-component-trims-md</h1>
 
     <!-- Description -->
     <h2>Description</h2>
-    <li>Issue</li>
-    <li>
+
+    <ul>
+        <li>Issue</li>
+        <li>Minal Reproduction</li>
+    </ul>
+
+    <!-- Issue -->
     <h2>Issue</h2>
+
     <p>
         Referencing a component in markdown results in the remaining markdown content not being rendered, including subsequent components.
     </p>
-    <h2>Markdown Content</h2>
+
+    <!-- Usage -->
+    <h2>Markdown Pages</h2>
+
+    <p>This markdown <a href="/mdc-prob">page</a> includes component, followed by additional content, which is being trimmed..</p>
+
     <ul>
         <li><a href="/basic">Basic Page</a></li>
         <li><a href="/mdc-prob">Problematic Markdown Page</a></li>
     </ul>
-  </div>
-</template>
+
+</div>
+
+</NuxtLayout>
+</div></template>
 <!--------@/pages/index.vue---------------------------------------------------->
